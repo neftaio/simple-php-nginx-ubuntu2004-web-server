bf88a0b1723d52e80a621b6d18e677c56e86b9ca
Neftali Forero <neftaiof@gmail.com>
2021-12-29 09:17:00 -0500

    Include certbot to installation

--- install.sh
+++ install.sh
@@ -20,8 +20,14 @@ done
 echo -e "-->Verifing os enabled system"
 os_enabled="ubuntu"
 os=`cat /etc/os-release | awk  -F '=' '{if ($1 == "ID") print $2}'`
-if [ $os != $os_enabled ]; then 
+if [ "$os" != "$os_enabled" ]; then 
     echo -e "OS $os not compatible with this script"
+    exit 1
+fi
+os_version_enabled="20.04"
+os_version=`cat /etc/os-release | awk -F '=' '{if ($1 == "VERSION_ID") print $2}'`
+if [ "$os_version" != "$os_version_enabled" ]; then
+    echo -e "OS version $os_version not valid to run this script"
     exit 1
 fi
 
@@ -47,7 +53,7 @@ fi
 tee -a /etc/nginx/sites-available/$domain <<EOF
 server {
     listen 80;
-    server_name your_domain www.$domain;
+    server_name $domain www.$domain;
     root /var/www/$domain;
 
     index index.html index.htm index.php;
@@ -58,7 +64,7 @@ server {
 
     location ~ \.php$ {
         include snippets/fastcgi-php.conf;
-        fastcgi_pass unix:/var/run/php/php8.1-fpm.sock;
+        fastcgi_pass unix:/var/run/php/php7.4-fpm.sock;
      }
 
     location ~ /\.ht {
@@ -70,3 +76,8 @@ ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled/
 unlink /etc/nginx/sites-available/default
 nginx -t
 systemctl reload nginx
+
+echo -e "-->Install Cerbot"
+apt install certbot python3-certbot-nginx
+certbot --nginx -d $domain -d www.$domain
+systemctl status certbot.timer
